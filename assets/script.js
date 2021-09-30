f158be8365b52515f397a013c2af7075644d84e4
Ben Camarillo <jtby97@gmail.com>
2021-09-29 21:29:12 -0500

    added local storage variable and functions to compare times

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,149 @@
 //TODO add local storage for the text bar. How can you save it all at one time and put it in local storage.
-eventContent.setItem("eventContent", "");
-document.getElementById("result").innerHTML = setItem.getItem("eventContent");
 
-var storage = document.getElementById('.storage');
-var textArea = document.getElementById('text');
-var savebutton = document.getElementById('.button');
+var now = moment();
+$("span").text(now.format('MMMM DD YYYY, h:mm:ss a'));
+var fakeTime = "Wed Sep 29 2021" + now + "GMT-0500";
 
+// var storage = document.getElementById('hourTime');
+// var textArea = $("text")
+// var savebutton = document.getElementById('saveBtn');
 
-function start();
+// savebutton.onabort('click', function() {
+//     console.log($(this));
+//     localStorage.setItem("textarea", textArea.val());
+//     console.log("click")
+// })
 
+// function start();
 
-// start/open pages
-// current day displayed in header
-// set up time blocks
-// color quordinate the time collumn, text collum, and save button. They all have to have the same this
-// save text from block and save in local storage. Set up local storage
-// referance 13-ins_Bootstrap-Layout
+localStorage.setItem = ["" , "" , "" , "" ,"" , "" ,"" , "" ,"" , "" ,"" , "" ,"" , "" ,"" , ""];
+
+function compareTimesNine() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesTen() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesEleven() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesTwelve() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesOne() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesTwo() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesThree() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesFour() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesFive() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
+
+function compareTimesSix() {
+    if (moment(now).isAfter(currentHour)){
+        console.log("after!");
+        five.addClass("past");
+    } else if (moment(now).isBefore(currentHour)) {
+        console.log("before");
+        five.addClass("future");
+    } else if (moment(now).isSame(currentHour)) {
+        console.log("same")
+        five.addClass("present time");
+    }
+}
